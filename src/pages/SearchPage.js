ceb6e1919430531924fb02563b436e6ca679a7ee
paystrup <90240787+paystrup@users.noreply.github.com>
2022-12-11 19:02:41 +0100

    search input fix

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import CategoryCarousel from '../components/CategoryCarousel';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function SearchPage() {
   // import copy translations from i18n
@@ -8,10 +9,15 @@ export default function SearchPage() {
 
   return (
     <section className='mt-20 mb-32'>
-      <div className='px-6'>
-        <input className='placeholder-primaryGray-700 border-solid border-[1px] border-primaryGray-700 w-full py-3 rounded-xl bg-primaryGray-900 bg-opacity-20'
-        type="text" id="search" name="search" placeholder={t("searchpage.searchPlaceholder")}
-        />
+      <div className='px-6 w-full'>
+        <div className='relative flex'>
+          <div className='flex items-center'>
+            <MagnifyingGlassIcon className='h-6 w-6 absolute ml-3'/>
+          </div>
+          <input className='placeholder-primaryGray-700 border-[1px] border-primaryGray-700 w-full py-3 rounded-xl bg-primaryGray-900 bg-opacity-20 pl-12 focus-within:text-primaryGray-900'
+          type="text" id="search" name="search" placeholder={t("searchpage.searchPlaceholder")}
+          />
+        </div>
 
         <div className='mt-5'>
           <h3 className="text-xl font-medium">
