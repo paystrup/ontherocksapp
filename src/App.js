e0ef6f3832ca72346dc252ea825aa0aa71876a80
mikkelbirkegaard <mikkel-mc@hotmail.com>
2022-12-11 19:40:09 +0100

    Framer motion animation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,155 @@
-import React from 'react';
-import './App.css';
-import TopNavigation from './components/TopNavigation';
-import BottomNavigation from './components/BottomNavigation';
-import { Routes, Route, Navigate } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import LikesPage from './pages/LikesPage';
-import ProfilePage from './pages/ProfilePage';
-import SearchPage from './pages/SearchPage';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import CocktailPage from './pages/CocktailPage';
-import EventPage from './pages/EventPage';
-import SplashPage from './pages/SplashPage'
-import LoginPage from './pages/LoginPage';
-import ArticlesPage from './pages/ArticlesPage';
-import CompetitionPage from './pages/CompetitionPage';
+import React from "react";
+import "./App.css";
+import TopNavigation from "./components/TopNavigation";
+import BottomNavigation from "./components/BottomNavigation";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LikesPage from "./pages/LikesPage";
+import ProfilePage from "./pages/ProfilePage";
+import SearchPage from "./pages/SearchPage";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import CocktailPage from "./pages/CocktailPage";
+import EventPage from "./pages/EventPage";
+import SplashPage from "./pages/SplashPage";
+import LoginPage from "./pages/LoginPage";
+import ArticlesPage from "./pages/ArticlesPage";
+import CompetitionPage from "./pages/CompetitionPage";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <ScrollToTop />
-      <TopNavigation />
-      <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/likes" element={<LikesPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/recipe/:id" element={<CocktailPage />} exact={true} />
-            <Route path="/events/:id" element={<EventPage />} exact={true} />
-            <Route path="/articles/:id" element={<ArticlesPage />} exact={true}/>    
-            <Route path="/competition/:id" element={<CompetitionPage/>} exact={true}/>        
-            <Route path="*" element={<Navigate to="/" />} />
-            <Route path="/splash" element={<SplashPage/>}/>
+      <AnimatePresence exitBeforeEnter>
+        <ScrollToTop />
+        <TopNavigation />
+        <Routes key={location.pathname} location={location}>
+          <Route
+            path="/"
+            element={
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1 }}
+              >
+                <HomePage />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1 }}
+              >
+                <SearchPage />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/likes"
+            element={
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1 }}
+              >
+                <LikesPage />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1 }}
+              >
+                <ProfilePage />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1 }}
+              >
+                <LoginPage />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/recipe/:id"
+            element={
+              <motion.div
+                initial={{ x: "200vw", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <CocktailPage />
+              </motion.div>
+            }
+            exact={true}
+          />
+          <Route
+            path="/events/:id"
+            element={
+              <motion.div
+                initial={{ x: "200vw", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <EventPage />
+              </motion.div>
+            }
+            exact={true}
+          />
+          <Route
+            path="/articles/:id"
+            element={
+              <motion.div
+                initial={{ x: "200vw", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <ArticlesPage />
+              </motion.div>
+            }
+            exact={true}
+          />
+          <Route
+            path="/competition/:id"
+            element={
+              <motion.div
+                initial={{ x: "200vw", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <CompetitionPage />
+              </motion.div>
+            }
+            exact={true}
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/splash" element={<SplashPage />} />
         </Routes>
+      </AnimatePresence>
       <BottomNavigation />
-
     </div>
   );
 }
--- src/pages/CocktailPage.js
+++ src/pages/CocktailPage.js
@@ -1,217 +1,256 @@
-// Inspiration from https://youtu.be/_7gdsAfFV9o 
-import React from 'react'
+// Inspiration from https://youtu.be/_7gdsAfFV9o
+import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "../firebaseConfig";
-import { BookmarkIcon, ClockIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline";
-import { useTranslation } from 'react-i18next';
-import TypeWriterEffect from '../components/TypeWriterEffect';
-import ArticlesFeatured from '../components/ArticlesFeatured'
+import {
+  BookmarkIcon,
+  ClockIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/24/outline";
+import { useTranslation } from "react-i18next";
+import TypeWriterEffect from "../components/TypeWriterEffect";
+import ArticlesFeatured from "../components/ArticlesFeatured";
 
 export default function CocktailPage() {
-    const { t, i18n } = useTranslation();
-    // fetch depending on i18n language chosen
-    const fetchLng = i18n.language;
-    
-    const [article, setArticle] = useState([]);
-    const params = useParams();
-    console.log(params); //Returns the slug-name of the url you're navigated to
-    const id = params.id; // and the ID
+  const { t, i18n } = useTranslation();
+  // fetch depending on i18n language chosen
+  const fetchLng = i18n.language;
 
-    // Fetch book data based on the id from the slug
-    // This way we don't have to loop through the array
-    // We can fetch directly from the ID in fireStore with queries
-    // Dependency array listens for a new ID and rerenders
+  const [article, setArticle] = useState([]);
+  const params = useParams();
+  console.log(params); //Returns the slug-name of the url you're navigated to
+  const id = params.id; // and the ID
 
-    // articles = our fireStore collection, id = the query
-    useEffect(() => {
-        const docRef = doc(db, fetchLng, id);
-        onSnapshot(docRef, (snapshot) => {
-            setArticle({ ...snapshot.data(), id: snapshot.id });
-        });
-    }, [id, fetchLng, t]);
+  // Fetch book data based on the id from the slug
+  // This way we don't have to loop through the array
+  // We can fetch directly from the ID in fireStore with queries
+  // Dependency array listens for a new ID and rerenders
+
+  // articles = our fireStore collection, id = the query
+  useEffect(() => {
+    const docRef = doc(db, fetchLng, id);
+    onSnapshot(docRef, (snapshot) => {
+      setArticle({ ...snapshot.data(), id: snapshot.id });
+    });
+  }, [id, fetchLng, t]);
 
   return (
-    <div className='text-primaryWhite mt-16 mb-32'>
-        <div 
-          className='h-96 rounded-b-[30px] flex items-end'
-          style={{
-                  backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 30%, rgba(0,0,0,1) 100%), url(${article?.image?.srcMin})`,
-                  backgroundPosition: "top",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat"
-              }}
-        >
-          <div className='flex gap-2 font-regular px-5 py-8'>
-            <p className='uppercase text-sm border-[1.2px] rounded-xl py-1 px-5'>{article?.taste?.title}</p>
-            <p className='uppercase text-sm border-[1.2px] rounded-xl py-1 px-5'>{article?.liqour?.type}</p>
-          </div>
-
+    <div className="text-primaryWhite mt-16 mb-32">
+      <div
+        className="h-96 rounded-b-[30px] flex items-end"
+        style={{
+          backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 30%, rgba(0,0,0,1) 100%), url(${article?.image?.srcMin})`,
+          backgroundPosition: "top",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="flex gap-2 font-regular px-5 py-8">
+          <p className="uppercase text-sm border-[1.2px] rounded-xl py-1 px-5">
+            {article?.taste?.title}
+          </p>
+          <p className="uppercase text-sm border-[1.2px] rounded-xl py-1 px-5">
+            {article?.liqour?.type}
+          </p>
         </div>
-        <div className='mt-14 px-5'>
-          <div className='flex justify-between'>
-            <div className='flex gap-1 items-center font-thin text-primaryYellow'>
-              <ClockIcon className='h-5 w-5'/>
-              <p>{article?.time} min</p>
-            </div>
-
-            <div className='flex gap-3'>
-              <div className='border-[1px] rounded-full p-1'>
-                <PaperAirplaneIcon className='h-6 w-6 -rotate-45'/>
-              </div>
-
-              <div className='border-[1px] rounded-full p-1'>
-                <BookmarkIcon className='h-6 w-6'/>
-              </div>
-            </div>
+      </div>
+      <div className="mt-14 px-5">
+        <div className="flex justify-between">
+          <div className="flex gap-1 items-center font-thin text-primaryYellow">
+            <ClockIcon className="h-5 w-5" />
+            <p>{article?.time} min</p>
           </div>
 
-          <div className='mt-7'>
-            <h2 className='text-5xl font-displayBook'>{article?.title}</h2>
-            <p className='text-base text-primaryGray-500 font-thin leading-relaxed mt-4 line-clamp-4'>
-              {article?.body}
-            </p>
-            <p>... Læs mere</p>
-            <div className='mt-6 font-thin text-md flex flex-col gap-2'>
-              <div className='flex justify-between'>
-                <p>{article?.ingredients?.liqour?.amount}</p>
-                <a className='font-regular uppercase w-5/6 underline underline-offset-4 decoration-primaryYellow' href={article?.ingredients?.liqour?.link}>{article?.ingredients?.liqour?.title}</a>
-              </div>
-
-              {article?.ingredients?.first &&
-                <div className='flex justify-between'>
-                  <p>{article?.ingredients?.first?.amount}</p>
-                  <p className='uppercase w-5/6'>{article?.ingredients?.first?.title}</p>
-                </div>
-              }
-
-              {article?.ingredients?.second &&
-                <div className='flex justify-between'>
-                  <p>{article?.ingredients?.second?.amount}</p>
-                  <p className='uppercase w-5/6'>{article?.ingredients?.second?.title}</p>
-                </div>
-              }
-
-              {article?.ingredients?.third &&
-                <div className='flex justify-between'>
-                  <p>{article?.ingredients?.third?.amount}</p>
-                  <p className='uppercase w-5/6'>{article?.ingredients?.third?.title}</p>
-                </div>
-              }
-
-              {article?.ingredients?.fourth &&
-                <div className='flex justify-between'>
-                  <p>{article?.ingredients?.fourth?.amount}</p>
-                  <p className='uppercase w-5/6'>{article?.ingredients?.fourth?.title}</p>
-                </div>
-              }
-
-              {article?.ingredients?.fifth &&
-                <div className='flex justify-between'>
-                  <p>{article?.ingredients?.fifth?.amount}</p>
-                  <p className='uppercase w-5/6'>{article?.ingredients?.fifth?.title}</p>
-                </div>
-              }
-
-              {article?.ingredients?.sixth &&
-                <div className='flex justify-between'>
-                  <p>{article?.ingredients?.sixth?.amount}</p>
-                  <p className='uppercase w-5/6'>{article?.ingredients?.sixth?.title}</p>
-                </div>
-              }
-
-              {article?.ingredients?.seventh &&
-                <div className='flex justify-between'>
-                  <p>{article?.ingredients?.seventh?.amount}</p>
-                  <p className='uppercase w-5/6'>{article?.ingredients?.seventh?.title}</p>
-                </div>
-              }
-              
+          <div className="flex gap-3">
+            <div className="border-[1px] rounded-full p-1">
+              <PaperAirplaneIcon className="h-6 w-6 -rotate-45" />
             </div>
-       
-            <h2 className='mt-10 text-3xl font-displayBook'>
-              {t("cocktailPage.recipeTitle")}
-            </h2>
 
-            <div className='mt-6 text-md flex flex-col gap-3'>
-              {article?.recipe?.recipe1 &&
-                <div className='flex justify-between'>
-                  <div className=' flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7'>
-                    <p>1</p>
-                  </div>
-                  <p className='w-5/6'>{article?.recipe?.recipe1}</p>
+            <div className="border-[1px] rounded-full p-1">
+              <BookmarkIcon className="h-6 w-6" />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-7">
+          <h2 className="text-5xl font-displayBook">{article?.title}</h2>
+          <p className="text-base text-primaryGray-500 font-thin leading-relaxed mt-4 line-clamp-4">
+            {article?.body}
+          </p>
+          <p>... Læs mere</p>
+          <div className="mt-6 font-thin text-md flex flex-col gap-2">
+            <div className="flex justify-between">
+              <p>{article?.ingredients?.liqour?.amount}</p>
+              <a
+                className="font-regular uppercase w-5/6 underline underline-offset-4 decoration-primaryYellow"
+                href={article?.ingredients?.liqour?.link}
+              >
+                {article?.ingredients?.liqour?.title}
+              </a>
+            </div>
+
+            {article?.ingredients?.first && (
+              <div className="flex justify-between">
+                <p>{article?.ingredients?.first?.amount}</p>
+                <p className="uppercase w-5/6">
+                  {article?.ingredients?.first?.title}
+                </p>
+              </div>
+            )}
+
+            {article?.ingredients?.second && (
+              <div className="flex justify-between">
+                <p>{article?.ingredients?.second?.amount}</p>
+                <p className="uppercase w-5/6">
+                  {article?.ingredients?.second?.title}
+                </p>
+              </div>
+            )}
+
+            {article?.ingredients?.third && (
+              <div className="flex justify-between">
+                <p>{article?.ingredients?.third?.amount}</p>
+                <p className="uppercase w-5/6">
+                  {article?.ingredients?.third?.title}
+                </p>
+              </div>
+            )}
+
+            {article?.ingredients?.fourth && (
+              <div className="flex justify-between">
+                <p>{article?.ingredients?.fourth?.amount}</p>
+                <p className="uppercase w-5/6">
+                  {article?.ingredients?.fourth?.title}
+                </p>
+              </div>
+            )}
+
+            {article?.ingredients?.fifth && (
+              <div className="flex justify-between">
+                <p>{article?.ingredients?.fifth?.amount}</p>
+                <p className="uppercase w-5/6">
+                  {article?.ingredients?.fifth?.title}
+                </p>
+              </div>
+            )}
+
+            {article?.ingredients?.sixth && (
+              <div className="flex justify-between">
+                <p>{article?.ingredients?.sixth?.amount}</p>
+                <p className="uppercase w-5/6">
+                  {article?.ingredients?.sixth?.title}
+                </p>
+              </div>
+            )}
+
+            {article?.ingredients?.seventh && (
+              <div className="flex justify-between">
+                <p>{article?.ingredients?.seventh?.amount}</p>
+                <p className="uppercase w-5/6">
+                  {article?.ingredients?.seventh?.title}
+                </p>
+              </div>
+            )}
+          </div>
+
+          <h2 className="mt-10 text-3xl font-displayBook">
+            {t("cocktailPage.recipeTitle")}
+          </h2>
+
+          <div className="mt-6 text-md flex flex-col gap-3">
+            {article?.recipe?.recipe1 && (
+              <div className="flex justify-between">
+                <div className=" flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7">
+                  <p>1</p>
                 </div>
-              }
+                <p className="w-5/6">{article?.recipe?.recipe1}</p>
+              </div>
+            )}
 
-              {article?.recipe?.recipe2 &&
-              <div className='flex justify-between'>
-                <div className=' flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7'>
+            {article?.recipe?.recipe2 && (
+              <div className="flex justify-between">
+                <div className=" flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7">
                   <p>2</p>
                 </div>
-                <p className='w-5/6'>{article?.recipe?.recipe2}</p>
+                <p className="w-5/6">{article?.recipe?.recipe2}</p>
               </div>
-              }
-              
-              {article?.recipe?.recipe3 &&
-              <div className='flex justify-between'>
-                <div className=' flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7'>
+            )}
+
+            {article?.recipe?.recipe3 && (
+              <div className="flex justify-between">
+                <div className=" flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7">
                   <p>3</p>
                 </div>
-                <p className='w-5/6'>{article?.recipe?.recipe3}</p>
+                <p className="w-5/6">{article?.recipe?.recipe3}</p>
               </div>
-              }
+            )}
 
-              {article?.recipe?.recipe4 &&
-              <div className='flex justify-between'>
-                <div className=' flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7'>
+            {article?.recipe?.recipe4 && (
+              <div className="flex justify-between">
+                <div className=" flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7">
                   <p>4</p>
                 </div>
-                <p className='w-5/6'>{article?.recipe?.recipe4}</p>
+                <p className="w-5/6">{article?.recipe?.recipe4}</p>
               </div>
-              }
+            )}
 
-              {article?.recipe?.recipe5 &&
-              <div className='flex justify-between'>
-                <div className=' flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7'>
+            {article?.recipe?.recipe5 && (
+              <div className="flex justify-between">
+                <div className=" flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7">
                   <p>5</p>
                 </div>
-                <p className='w-5/6'>{article?.recipe?.recipe5}</p>
+                <p className="w-5/6">{article?.recipe?.recipe5}</p>
               </div>
-              }
+            )}
 
-              {article?.recipe?.recipe6 &&
-              <div className='flex justify-between'>
-                <div className=' flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7'>
+            {article?.recipe?.recipe6 && (
+              <div className="flex justify-between">
+                <div className=" flex items-center justify-center border-[1px] rounded-full p-1 h-7 w-7">
                   <p>6</p>
                 </div>
-                <p className='w-5/6'>{article?.recipe?.recipe6}</p>
+                <p className="w-5/6">{article?.recipe?.recipe6}</p>
               </div>
-              }
-            </div>
-          </div>
-
-          <div className='mt-10'>
-            <button className="text-primaryYellow border-[1px] w-full py-3 rounded-xl">
-            {t("cocktailPage.addflavorBtn")}
-            </button>
-          </div>
-
-          <div className='mt-10'>
-            <h3 className="text-xl font-medium">{t("cocktailPage.carouselTitle")}</h3>
-            <p>Carousel her</p>
-          </div>
-
-          <div className='mt-10'>
-            <h3 className="text-xl font-medium">{t("cocktailPage.carouselTitle2")} {article?.liqour?.type}</h3>
-            <p>Carousel her</p>
-          </div>
-
-          <div className='mt-14'>
-            <TypeWriterEffect words={['din kæreste', 'din hund', 'din håndværker', 'dit postbud', 'din ven', 'din morfar','din kollega']} />
+            )}
           </div>
         </div>
-        <ArticlesFeatured />
+
+        <div className="mt-10">
+          <button className="text-primaryYellow border-[1px] w-full py-3 rounded-xl">
+            {t("cocktailPage.addflavorBtn")}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <h3 className="text-xl font-medium">
+            {t("cocktailPage.carouselTitle")}
+          </h3>
+          <p>Carousel her</p>
+        </div>
+
+        <div className="mt-10">
+          <h3 className="text-xl font-medium">
+            {t("cocktailPage.carouselTitle2")} {article?.liqour?.type}
+          </h3>
+          <p>Carousel her</p>
+        </div>
+
+        <div className="mt-14">
+          <TypeWriterEffect
+            words={[
+              "din kæreste",
+              "din hund",
+              "din håndværker",
+              "dit postbud",
+              "din ven",
+              "din morfar",
+              "din kollega",
+            ]}
+          />
+        </div>
+      </div>
+      <ArticlesFeatured />
     </div>
-  )
+  );
 }
