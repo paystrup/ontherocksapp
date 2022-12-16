5f07421066f00f4d2e014beb61a939dae10b9230
mikkelbirkegaard <mikkel-mc@hotmail.com>
2022-12-17 00:37:44 +0100

    Onboarding steps (done, men mangler styling)

--- src/App.js
+++ src/App.js
@@ -17,26 +17,31 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import ScrollToTopBtn from "./components/ScrollToTopBtn";
 import Footer from "./components/Footer";
-import OnboardingSteps from "./components/AgeConfirmed";
+import { Onboarding } from "./components/Onboarding";
+import AgeConfirmed from "./components/AgeConfirmed";
 
 function App() {
-
   return (
     <div className="App">
-      <OnboardingSteps />
+      <AgeConfirmed />
+      <Onboarding />
       <ToastContainer theme="dark" autoClose={3000} />
       <TopNavigation />
       <ScrollToTopBtn />
       <Routes>
-        <Route path="/" element={<HomePage />}/>
+        <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/likes"element={<LikesPage />}/>
+        <Route path="/likes" element={<LikesPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/recipe/:id" element={<CocktailPage />} exact={true} />
         <Route path="/events/:id" element={<EventPage />} exact={true} />
         <Route path="/articles/:id" element={<ArticlesPage />} exact={true} />
-        <Route path="/competition/:id" element={<CompetitionPage />} exact={true} />
+        <Route
+          path="/competition/:id"
+          element={<CompetitionPage />}
+          exact={true}
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <ScrollToTop />
--- src/pages/OnboardingPage.js
+++ src/pages/OnboardingPage.js
@@ -1,46 +1,40 @@
-import React from 'react'
-import { useState } from 'react';
-import MainButton from '../components/MainButton';
+import React from "react";
+import { useState } from "react";
+import { Navigate } from "react-router";
+import MainButton from "../components/MainButton";
 
 export default function OnboardingPage() {
-    const [isConfirmed, setIsConfirmed] = useState(false);
+  const [isConfirmed, setIsConfirmed] = useState(false);
 
-    const handleAgeConfirmed = (event) => {
-        setIsConfirmed(true);
-        console.log(isConfirmed);
-    }
+  const handleAgeConfirmed = (event) => {
+    setIsConfirmed(true);
+    console.log(isConfirmed);
+  };
 
-    const handleAgeConfirmedDelete = (event) => {
-        setIsConfirmed(false);
-        console.log(isConfirmed);
-    }
+  const handleAgeConfirmedDelete = (event) => {
+    setIsConfirmed(false);
+    console.log(isConfirmed);
+  };
 
-    return (
-        <>
-            {isConfirmed && (
-                <section className='fixed h-full w-full top-0 left-0 bg-primaryGray-700 z-[9999]'>
-                    <h2 className='font-displayBook text-7xl text-center'>Er du over 18 år?</h2>
-                    <MainButton />
+  return (
+    <>
+      {isConfirmed && (
+        <section className="fixed h-full w-full top-0 left-0 bg-primaryGray-700 z-[9999]">
+          <h2 className="font-displayBook text-7xl text-center">
+            Er du over 18 år?
+          </h2>
+          <MainButton />
 
-                    <div className='flex gap-5'>
-                        <button 
-                            onClick={handleAgeConfirmed()}
-                            className="border-2"
-                        >
-                            Klik her for at confirme
-
-                        </button>
-
-                        <button 
-                            onClick={handleAgeConfirmedDelete()}
-                            className="border-2"
-                        >
-                            Klik her for at slette
-
-                        </button>
-                    </div>
-                </section>
-            )}
-        </>
-    )
+          <div className="flex gap-5">
+            <button onClick={handleAgeConfirmed()} className="border-2">
+              Klik her for at confirme
+            </button>
+            <button onClick={handleAgeConfirmedDelete()} className="border-2">
+              Klik her for at slette
+            </button>
+          </div>
+        </section>
+      )}
+    </>
+  );
 }
